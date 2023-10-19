af38fd9717f8e791464a8ea9075149988df447c8
Daniela Moreno <dani9292@hotmail.com>
2023-10-19 16:30:35 -0500

    getting values from html

--- scrapecurrency.py
+++ scrapecurrency.py
@@ -5,8 +5,10 @@ def get_curreny(in_currency, out_currency):
     url = f'https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1'
     content = requests.get(url).text
     soup = BeautifulSoup(content, 'html.parser')
-    soup.find("span", class_="ccOutputRslt")
+    rate = soup.find("span", class_="ccOutputRslt").get_text()
+    rate = float(rate[:-4])
 
-    print(content)
+    #print(rate)
+    return rate
 
-get_curreny('INR', 'USD')
+print(get_curreny('INR', 'USD'))
