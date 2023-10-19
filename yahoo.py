ff10509fe25517c27448140299cee0ba7b8d2453
Daniela Moreno <dani9292@hotmail.com>
2023-10-19 15:11:34 -0500

    ectracting data

--- yahoo.py
+++ yahoo.py
@@ -1,14 +1,28 @@
-import requests 
+import requests
+from datetime import datetime
+import time
 
+# Get dynamic values from the user
+ticker = input("Enter ticker symbol: ")
+from_date = input('Enter start date in yyyy/mm/dd format: ')
+to_date = input('Enter end date in yyyy/mm/dd format: ')
 
-url = "https://query1.finance.yahoo.com/v7/finance/download/NFLX?period1=1666209500&period2=1697745500&interval=1d&events=history&includeAdjustedClose=true"
+from_datetime = datetime.strptime(from_date, '%Y/%m/%d')
+to_datetime = datetime.strptime(to_date, '%Y/%m/%d')
+
+from_epoch = int(time.mktime(from_datetime.timetuple()))
+to_epoch = int(time.mktime(to_datetime.timetuple()))
+
+url = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={from_epoch}&period2={to_epoch}&interval=1d&events=history&includeAdjustedClose=true"
 
 headers = { "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/109.0"}
 
-
+print(f"Fetching data for ticker: {ticker}")
+print(f"From Date: {from_date}")
+print(f"To Date: {to_date}")
 
 content = requests.get(url, headers=headers).content
-#print(content)
+print(content)
 
 with open("data.csv", "wb") as file:
-    file.write(content)# download binary files , any type of content
+    file.write(content)
